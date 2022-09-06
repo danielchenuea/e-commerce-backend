026d047278e803afd4cdbd99bb3c2a34c3af371a
danielchenuea <dac.eng17@uea.edu.br>
2022-09-05 22:47:32 -0300

    7: heroku deploy

--- app/config/mongoconnection.js
+++ app/config/mongoconnection.js
@@ -1,6 +1,6 @@
 var Mongoose = require('mongoose')
 
 // Conexões mongo/mongoose para conectar com o banco de dados mongodb
-Mongoose.connect('process.env.MONGODB_URI');
+Mongoose.connect(process.env.MONGODB_URI);
 Mongoose.connection.on('connected', () => console.log('Connected to database'));
 Mongoose.connection.on('error', (err) => console.log('Connection failed with - ',err));
