6896c7d0fadc4f899cf12c30ad734e8ca5074392
danielchenuea <dac.eng17@uea.edu.br>
2022-09-06 18:03:41 -0300

    9: fix connections

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require('./app/config/server')
 
-// app.listen(process.env.PORT,() => {
-app.listen(3001,() => {
+app.listen(process.env.PORT,() => {
+// app.listen(3001,() => {
   console.log("Servidor ON")
 })
